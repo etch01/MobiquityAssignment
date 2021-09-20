a3fd86470728f6bdc1983902da829e3cd5ad3a4e
Hassan Emad <>
2021-09-20 11:06:09 +0200

    Init

---
+++ ios/Pods/Target Support Files/Pods-mobiquityChallenge-mobiquityChallengeTests/Pods-mobiquityChallenge-mobiquityChallengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mobiquityChallenge_mobiquityChallengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_mobiquityChallenge_mobiquityChallengeTests
+@end
